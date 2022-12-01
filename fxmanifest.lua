35ec42efc859957d3d924fb44484403c2b7160ef
PrivateGiles <56416493+PrivateGiles@users.noreply.github.com>
2022-12-01 20:09:27 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'PrivateGiles'
+description 'Server Utilities for ESX Servers - Remove the unwanted junk!'
+
+version '1.0.0'
+
+lua54 'yes'
+
+client_scripts {
+    'client/*.lua',
+}
+
+server_scripts {
+    'server/*.lua',
+}
+
